dc43e3e40a22fcf6bd5c1bdd88d6a59fcf3b8c41
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-08 01:24:18 +0000

    Use Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/app";
+import { Inter } from "@next/font/google";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
@@ -6,14 +7,18 @@ import { api } from "utils/api";
 
 import "styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div className={inter.className}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   );
 };
 
