1bbc58ec22a2f37ef96bb08e966842407d7c4dae
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-15 05:59:40 +0000

    WIP: Add components/algolia

---
+++ src/components/algolia/NoResultsBoundary.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+import { useInstantSearch } from "react-instantsearch-hooks-web";
+
+export type NoResultsBoundaryProps = {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+};
+
+/**
+ * @see https://www.algolia.com/doc/guides/building-search-ui/going-further/conditional-display/react-hooks/#handling-no-results
+ */
+export default function NoResultsBoundary({
+  fallback,
+  children,
+}: NoResultsBoundaryProps) {
+  const { results } = useInstantSearch();
+
+  // The `__isArtificial` flag makes sure not to display the No Results message
+  // when no hits have been returned yet.
+  if (!results.__isArtificial && results.nbHits === 0) {
+    return (
+      <>
+        {fallback}
+        <div hidden>{children}</div>
+      </>
+    );
+  }
+
+  return <>{children}</>;
+}
+
+export function NoResults() {
+  const { indexUiState } = useInstantSearch();
+
+  return (
+    <div>
+      <p>
+        No results for <q>{indexUiState.query}</q>.
+      </p>
+    </div>
+  );
+}
