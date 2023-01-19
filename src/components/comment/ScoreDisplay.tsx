53c6d335b97d5d6a16bf61606484cb10acbb9da3
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-19 20:43:51 +0000

    Rename handle_vote -> on_vote

--- src/components/comment/ScoreDisplay.tsx
+++ src/components/comment/ScoreDisplay.tsx
@@ -6,27 +6,29 @@ export type ScoreDisplayProps = {
   score: number;
   userUpvoted: boolean;
   userDownvoted: boolean;
-  handleUpvote?: () => void;
-  handleDownvote?: () => void;
+  onUpvote?: () => void;
+  onDownvote?: () => void;
 };
 
 export default function ScoreDisplay({
   score,
   userUpvoted,
   userDownvoted,
-  handleUpvote,
-  handleDownvote,
+  onUpvote,
+  onDownvote,
 }: ScoreDisplayProps) {
   const { data: session } = useSession();
 
   const signedIn = session?.user != null;
+
+  // TODO: handle handleUpvote and handleDownvote being undefined in title
 
   return (
     // TODO: icons that are filled
     <div className="-mr-2 flex w-12 flex-col items-center gap-y-0.5 text-lg">
       <button
         type="button"
-        onClick={handleUpvote}
+        onClick={onUpvote}
         title={
           signedIn
             ? userUpvoted
@@ -38,7 +40,7 @@ export default function ScoreDisplay({
           userUpvoted ? "text-green-500" : "text-gray-500",
           !signedIn && "cursor-not-allowed"
         )}
-        disabled={!signedIn || handleDownvote === undefined}
+        disabled={!signedIn || onUpvote === undefined}
       >
         <BiUpvote />
       </button>
@@ -47,7 +49,7 @@ export default function ScoreDisplay({
 
       <button
         type="button"
-        onClick={handleDownvote}
+        onClick={onDownvote}
         title={
           signedIn
             ? userDownvoted
@@ -59,7 +61,7 @@ export default function ScoreDisplay({
           userDownvoted ? "text-red-500" : "text-gray-500",
           !signedIn && "cursor-not-allowed"
         )}
-        disabled={!signedIn || handleDownvote === undefined}
+        disabled={!signedIn || onDownvote === undefined}
       >
         <BiDownvote />
       </button>
