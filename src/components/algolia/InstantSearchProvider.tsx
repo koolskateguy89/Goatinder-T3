a044e7f93be4e246975b17398eb58e4da3d26c9a
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-19 21:18:10 +0000

    Move algolia client instantiation to utils

--- src/components/algolia/InstantSearchProvider.tsx
+++ src/components/algolia/InstantSearchProvider.tsx
@@ -1,14 +1,10 @@
-import algoliasearch from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch-hooks-web";
 
-import { env } from "env/client.mjs";
+import { searchClient } from "utils/algolia";
 
 // TODO: routing thingy to change URL
 // algolia instant search: https://www.algolia.com/doc/guides/building-search-ui/what-is-instantsearch/react-hooks/
-const searchClient = algoliasearch(
-  env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-  env.NEXT_PUBLIC_ALGOLIA_API_KEY
-);
+// might have to instantiate client here instead of in utils/algolia
 
 export type InstantSearchProviderProps = {
   children: React.ReactNode;
---
+++ src/utils/algolia.ts
@@ -0,0 +1,14 @@
+import algoliasearch from "algoliasearch";
+import algoliasearchlite from "algoliasearch/lite";
+
+import { env } from "env/client.mjs";
+
+export const searchClient = algoliasearchlite(
+  env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  env.NEXT_PUBLIC_ALGOLIA_API_KEY
+);
+
+export const fullClient = algoliasearch(
+  env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  env.NEXT_PUBLIC_ALGOLIA_API_KEY
+);
