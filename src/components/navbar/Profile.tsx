d91940a365530ad0859f418357854e10d523b375
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-22 01:52:37 +0000

    Use HeadlessUI for profile dropdown

--- src/components/navbar/Profile.tsx
+++ src/components/navbar/Profile.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import { Menu, Transition } from "@headlessui/react";
 
 import Avatar from "components/Avatar";
 
@@ -8,7 +10,7 @@ export default function Profile() {
   const { data: session, status } = useSession();
 
   // TODO: use daisyUI loading shiny thingy(?)
-  // should never show tho, since we're passing session to _app with getServerSideProps on every page
+  // should never show, because we're passing session to _app with getServerSideProps on every page
   if (status === "loading") {
     return (
       <button type="button" className="btn-primary btn-sm btn md:btn-md">
@@ -25,15 +27,10 @@ export default function Profile() {
     );
   }
 
-  const handleMenuClick = () => {
-    // only problem is that the transition on close is too quick
-    (document.activeElement as HTMLElement | null)?.blur();
-  };
-
   return (
     // maybe use a modal instead of dropdown on mobile?
-    <div className="dropdown-end dropdown" aria-haspopup="menu">
-      <label tabIndex={0} className="flex cursor-pointer">
+    <Menu as="div" className="relative flex">
+      <Menu.Button className="my-auto h-10">
         <Avatar
           image={session.user.image}
           name={session.user.name}
@@ -43,30 +40,34 @@ export default function Profile() {
             sizes: "2.5rem",
           }}
         />
-      </label>
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu rounded-box w-52 bg-base-200 p-2 shadow"
-        role="menu"
-        onClick={handleMenuClick}
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
       >
-        <li>
-          <span>Item 1</span>
-        </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li>
-          <button
-            type="button"
-            onClick={() => signOut()}
-            className="font-semibold"
-          >
-            Sign out
-          </button>
-        </li>
-      </ul>
-    </div>
+        <Menu.Items
+          as="ul"
+          className="menu rounded-box absolute right-0 top-full mt-1 w-52 origin-top-right bg-base-200 p-2 shadow"
+        >
+          <Menu.Item as="li">
+            {({ close }) => (
+              <Link href="/profile" onClick={close}>
+                Profile
+              </Link>
+            )}
+          </Menu.Item>
+          <Menu.Item as="li">
+            <button type="button" onClick={() => signOut()}>
+              Sign out
+            </button>
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
