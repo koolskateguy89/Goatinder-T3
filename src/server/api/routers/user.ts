7bc2651f74874260a8e0a978a2d296e8b4d8b5e5
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-22 00:33:40 +0000

    Add `userRouter`

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  createProfile: protectedProcedure
+    .input(
+      z.object({
+        bio: z.string().trim().min(1),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { bio } = input;
+      const userId = ctx.session.user.id;
+
+      await ctx.prisma.profile.upsert({
+        where: {
+          userId,
+        },
+        update: {
+          bio,
+        },
+        create: {
+          userId,
+          bio,
+        },
+      });
+    }),
+});
