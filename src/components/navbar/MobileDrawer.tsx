5f20ee3051abe867e3e74a7754c255a3faa1ed26
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-01-22 23:55:32 +0000

    Use HeadlessUI for MobileDrawer

--- src/components/navbar/MobileDrawer.tsx
+++ src/components/navbar/MobileDrawer.tsx
@@ -1,12 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+import { Fragment, useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Dialog, Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { HiXMark } from "react-icons/hi2";
 
-import ThemeSwitcher from "components/ThemeSwitcher";
 import Brand from "components/Brand";
+import ThemeSwitcher from "components/ThemeSwitcher";
+
+/*
+TODO?: add Avatar & name, similar to Meraki UI's sidebar example: https://merakiui.com/components/sidebar
+*/
 
 function MobileNavItem({
   href,
@@ -25,125 +30,121 @@ function MobileNavItem({
 }
 
 // TODO: add icons
-function Drawer({
-  open,
-  closeDrawer,
-}: {
-  open: boolean;
-  closeDrawer: (e: React.MouseEvent) => void;
-}) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
-
-  return createPortal(
-    // TODO: trap focus inside this
-    // TODO: improve accessibility?
-    // TODO: keyboard navigable - currently hard to get inside the drawer
-    <div
-      className={clsx(
-        "group/drawer fixed inset-0 z-50 overflow-y-auto lg:hidden",
-        !open && "invisible"
-      )}
-      tabIndex={0}
-      role="dialog"
-      aria-modal
-    >
-      {/* backdrop blur */}
-      <div
-        onClick={closeDrawer}
-        // motion-safe: transition 150ms to transparent on close after 275ms delay (waiting for sidebar to close)
-        className="fixed inset-0 bg-neutral/40 group-[.invisible]/drawer:bg-transparent motion-safe:group-[.invisible]/drawer:delay-[275ms] motion-safe:group-[.invisible]/drawer:duration-150"
-        aria-hidden
-      />
-
-      {/* drawer */}
-      <aside
-        // using clsx to logically group classnames because prettier basically scrambles them
-        className={clsx(
-          "relative -ml-80 min-h-screen w-80 bg-base-100 [&>*]:bg-inherit",
-          "-ml-80 group-[:not(.invisible)]/drawer:ml-0",
-          "motion-safe:transition-all motion-safe:group-[:not(.invisible)]/drawer:delay-100" // only delay on open
-        )}
-      >
-        {/* drawer content here */}
-
-        <header className="flex items-center gap-1 border-b border-base-300 px-4 pb-4 pt-3 dark:border-white/20">
-          <Link href="/" className="btn-ghost btn-sm btn text-xl normal-case">
-            <Brand />
-          </Link>
-
-          {/* TODO: hide the ThemeSwitcher in navbar [below lg] */}
-          <ThemeSwitcher />
-
-          {/* TODO: add more icons here */}
-
-          {/* TODO: close button icon on far right */}
-          <button
-            type="button"
-            onClick={closeDrawer}
-            className="btn-ghost btn-sm btn ml-auto text-xl"
-          >
-            X
-          </button>
-        </header>
-
-        <main>
-          <nav>
-            <ul className="menu my-4 px-4">
-              <li className="menu-title">
-                <span>...</span>
-              </li>
-              <MobileNavItem href="/">Home</MobileNavItem>
-              <li className="menu-title">
-                <span>Shoes</span>
-              </li>
-              <MobileNavItem href="/shoes/search">Search</MobileNavItem>
-              <MobileNavItem href="/shoes/explore">Explore</MobileNavItem>
-              <li className="menu-title">
-                <span>...</span>
-              </li>
-              <MobileNavItem href="/contact">Contact</MobileNavItem>
-              <MobileNavItem href="/about">About</MobileNavItem>
-              {Array.from({ length: 20 }).map((_, i) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={i}>
-                  <span>{i + 1}</span>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </main>
-      </aside>
-    </div>,
-    document.body
-  );
-}
-
 export default function MobileDrawer() {
   const router = useRouter();
 
-  const [open, setOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = useCallback(() => setOpen((isOpen) => !isOpen), []);
-  const closeDrawer = useCallback(() => setOpen(false), []);
+  useEffect(() => setIsOpen(false), [router.asPath]);
 
-  useEffect(() => setOpen(false), [router.asPath]);
+  const closeModal = () => setIsOpen(false);
+
+  const openModal = () => setIsOpen(true);
 
   return (
     <>
       <button
         type="button"
-        onClick={handleClick}
+        onClick={openModal}
         className="navbar-icon-btn btn-primary btn lg:hidden"
       >
         <span>
           <HiOutlineMenuAlt1 />
         </span>
       </button>
-      <Drawer open={open} closeDrawer={closeDrawer} />
+
+      <Transition show={isOpen} as={Fragment}>
+        <Dialog onClose={closeModal} className="relative z-40">
+          {/* The backdrop, rendered as a fixed sibling to the panel container */}
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out motion-safe:duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in motion-safe:duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/30" aria-hidden />
+          </Transition.Child>
+
+          {/* Full-screen scrollable container */}
+          <div className="fixed inset-0 overflow-y-auto">
+            {/* Container to handle height */}
+            <div className="h-screen h-[100dvh]">
+              <Transition.Child
+                as={Fragment}
+                enter="relative ease-out motion-safe:duration-300"
+                enterFrom="opacity-80 -translate-x-full"
+                enterTo="opacity-100 translate-0"
+                leave="ease-in motion-safe:duration-200"
+                leaveFrom="opacity-100 translate-0"
+                leaveTo="opacity-80 -translate-x-full"
+              >
+                <Dialog.Panel as="aside" className="w-80 bg-base-100">
+                  <Dialog.Title className="sr-only">
+                    Sidebar with main navigation
+                  </Dialog.Title>
+
+                  <header className="navbar gap-1 border-b border-base-300 px-4 dark:border-white/10">
+                    <Link
+                      href="/"
+                      className="btn-ghost btn-sm btn text-xl normal-case"
+                    >
+                      <Brand />
+                    </Link>
+
+                    {/* TODO?: hide the ThemeSwitcher in navbar [below lg, i.e. when this can be opened] */}
+                    <ThemeSwitcher />
+                    {/* TODO?: add more icons here */}
+
+                    <button
+                      type="button"
+                      onClick={closeModal}
+                      className="navbar-icon-btn ml-auto"
+                    >
+                      <span>
+                        <HiXMark />
+                      </span>
+                    </button>
+                  </header>
+
+                  <main>
+                    <nav>
+                      <ul className="menu my-4 px-4">
+                        <li className="menu-title">
+                          <span>...</span>
+                        </li>
+                        <MobileNavItem href="/">Home</MobileNavItem>
+                        <li className="menu-title">
+                          <span>Shoes</span>
+                        </li>
+                        <MobileNavItem href="/shoes/search">
+                          Search
+                        </MobileNavItem>
+                        <MobileNavItem href="/shoes/explore">
+                          Explore
+                        </MobileNavItem>
+                        <li className="menu-title">
+                          <span>...</span>
+                        </li>
+                        <MobileNavItem href="/contact">Contact</MobileNavItem>
+                        <MobileNavItem href="/about">About</MobileNavItem>
+                        {Array.from({ length: 20 }).map((_, i) => (
+                          // eslint-disable-next-line react/no-array-index-key
+                          <li key={i}>
+                            <span>{i + 1}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </nav>
+                  </main>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 }
