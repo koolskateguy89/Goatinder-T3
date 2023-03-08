2299afb9f14025e5afc1114bfba3fd5cb05f86b0
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-03-08 16:03:43 +0000

    Fix styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ export type LayoutProps = React.PropsWithChildren;
 
 export default function Container({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen min-h-[100dvh] flex-col">
+    <div className="flex min-h-screen !min-h-[100dvh] flex-col">
       <div className="sticky top-0 z-30 w-full flex-none border-b border-base-300 bg-transparent backdrop-blur transition-colors dark:border-white/5">
         <NavBar />
       </div>
