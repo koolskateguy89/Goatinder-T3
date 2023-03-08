6b4f54aa8a44d283b6d3040e5af951c011e50ad1
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-03-08 03:00:53 +0000

    Split chat into a component, and handle messaging yourself

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,110 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import { type ImmerReducer, useImmerReducer } from "use-immer";
+
+import { api, type RouterOutputs } from "utils/api";
+import ChatMeta from "components/chat/ChatMeta";
+import Messages from "components/chat/Messages";
+import NewMessageForm from "components/chat/NewMessageForm";
+
+type MessageType = RouterOutputs["chat"]["messagesById"]["messages"][number];
+
+type MessageState = {
+  messages: MessageType[];
+};
+
+type MessageAction =
+  | { type: "set"; payload: MessageType[] }
+  | { type: "add"; payload: MessageType }
+  | { type: "delete"; payload: { id: string } };
+
+// This _may_ get complicated if using websockets for real-time updates
+const messageReducer: ImmerReducer<MessageState, MessageAction> = (
+  draft,
+  action
+) => {
+  switch (action.type) {
+    case "set":
+      draft.messages = action.payload;
+      break;
+    case "add":
+      draft.messages.push(action.payload);
+      break;
+    case "delete":
+      draft.messages = draft.messages.filter(
+        (message) => message.id !== action.payload.id
+      );
+      break;
+    default:
+      break;
+  }
+};
+
+export default function Chat({ id }: { id: string }) {
+  // prefetched in getServerSideProps
+  const { data: chatData } = api.chat.infoById.useQuery({ id });
+  const { data: messagesData } = api.chat.messagesById.useQuery({ id });
+
+  const title = chatData ? `${chatData.name} - goaTinder` : "goaTinder";
+
+  const [messageState, messagesDispatch] = useImmerReducer(messageReducer, {
+    messages: messagesData?.messages ?? [],
+  });
+
+  useEffect(() => {
+    if (messagesData) {
+      messagesDispatch({
+        type: "set",
+        payload: messagesData.messages,
+      });
+    }
+  }, [messagesDispatch, messagesData]);
+
+  const onMessageDelete = (messageId: string) => {
+    messagesDispatch({ type: "delete", payload: { id: messageId } });
+  };
+
+  const onMessageSent = (message: MessageType) => {
+    messagesDispatch({ type: "add", payload: message });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="border-b-4 border-base-300 p-2 dark:border-white/5">
+        {chatData ? (
+          <ChatMeta
+            name={chatData.name}
+            image={chatData.image}
+            nameHref={
+              chatData.groupChat ? `/chat/${id}/manage` : `/profile/${id}`
+            }
+          />
+        ) : (
+          <div>
+            <h1>loading...</h1>
+          </div>
+        )}
+      </div>
+
+      {chatData ? (
+        <div className="p-1 md:p-4">
+          <Messages
+            groupChat={chatData.groupChat}
+            messages={messageState.messages}
+            onDelete={onMessageDelete}
+          />
+          <NewMessageForm
+            id={chatData.id}
+            groupChat={chatData.groupChat}
+            onMessageSent={onMessageSent}
+          />
+        </div>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
+  );
+}
--- src/pages/chat/[id]/index.tsx
+++ src/pages/chat/[id]/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import type {
   GetServerSideProps,
   InferGetServerSidePropsType,
@@ -6,88 +5,21 @@ import type {
 } from "next";
 import Head from "next/head";
 import { getServerSession } from "next-auth";
-import { type ImmerReducer, useImmerReducer } from "use-immer";
 
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import { createSSGHelpers } from "utils/ssg";
-import { api, type RouterOutputs } from "utils/api";
 import Sidebar from "components/chat/Sidebar";
-import ChatMeta from "components/chat/ChatMeta";
-import Messages from "components/chat/Messages";
-import NewMessageForm from "components/chat/NewMessageForm";
+import Chat from "components/chat/Chat";
 
-// gc1: cleyua3uq000hw26kqttq4o5x
+// gc1: clez36kha000hw2govvab9yax
 
-type MessageType = RouterOutputs["chat"]["messagesById"]["messages"][number];
-
-type MessageState = {
-  messages: MessageType[];
-};
-
-type MessageAction =
-  | { type: "set"; payload: MessageType[] }
-  | { type: "add"; payload: MessageType }
-  | { type: "delete"; payload: { id: string } };
-
-// This _may_ get complicated if using websockets for real-time updates
-const messageReducer: ImmerReducer<MessageState, MessageAction> = (
-  draft,
-  action
-) => {
-  switch (action.type) {
-    case "set":
-      draft.messages = action.payload;
-      break;
-    case "add":
-      draft.messages.push(action.payload);
-      break;
-    case "delete":
-      draft.messages = draft.messages.filter(
-        (message) => message.id !== action.payload.id
-      );
-      break;
-    default:
-      break;
-  }
-};
-
-// This will show the chat for both a PM and GC, with a like sidebar/side content of all their chats (PMs & GCs)
 const ChatPage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ id }) => {
-  // TODO: use id for showing active in side thingy, probably not in this component
-
-  // prefetched in getServerSideProps
-  const { data: chatData } = api.chat.infoById.useQuery({ id });
-  const { data: messagesData } = api.chat.messagesById.useQuery({ id });
-
-  const title = chatData ? `${chatData.name} - goaTinder` : "goaTinder";
-
-  const [messageState, messagesDispatch] = useImmerReducer(messageReducer, {
-    messages: messagesData?.messages ?? [],
-  });
-
-  useEffect(() => {
-    if (messagesData) {
-      messagesDispatch({
-        type: "set",
-        payload: messagesData.messages,
-      });
-    }
-  }, [messagesDispatch, messagesData]);
-
-  const onMessageDelete = (messageId: string) => {
-    messagesDispatch({ type: "delete", payload: { id: messageId } });
-  };
-
-  const onMessageSent = (message: MessageType) => {
-    messagesDispatch({ type: "add", payload: message });
-  };
-
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>Chat - goaTinder</title>
       </Head>
       <main className="container flex pb-4">
         <div className="min-h-screen min-h-[calc(100dvh-4rem)] basis-1/4 overflow-auto border-r-4 border-base-300 dark:border-white/5 max-md:hidden">
@@ -96,37 +28,12 @@ const ChatPage: NextPage<
           </div>
         </div>
         <div className="flex-grow">
-          <div className="border-b-4 border-base-300 p-2 dark:border-white/5">
-            {chatData ? (
-              <ChatMeta
-                name={chatData.name}
-                image={chatData.image}
-                nameHref={
-                  chatData.groupChat ? `/chat/${id}/manage` : `/profile/${id}`
-                }
-              />
-            ) : (
-              <div>
-                <h1>loading...</h1>
-              </div>
-            )}
-          </div>
-
-          {chatData ? (
-            <div className="p-1 md:p-4">
-              <Messages
-                groupChat={chatData.groupChat}
-                messages={messageState.messages}
-                onDelete={onMessageDelete}
-              />
-              <NewMessageForm
-                id={chatData.id}
-                groupChat={chatData.groupChat}
-                onMessageSent={onMessageSent}
-              />
-            </div>
+          {id ? (
+            <Chat id={id} />
           ) : (
-            <>Loading...</>
+            <div className="flex items-center justify-center pt-4">
+              You cannot message yourself!
+            </div>
           )}
         </div>
       </main>
@@ -147,13 +54,19 @@ export const getServerSideProps = (async (context) => {
       },
     };
 
-  // TODO: handle them trying to message themself
-
   const id = context.params?.id;
 
   if (!id)
     return {
       notFound: true,
+    };
+
+  // Trying to message yourself
+  if (id === session.user.id)
+    return {
+      props: {
+        session,
+      },
     };
 
   const ssg = await createSSGHelpers(session);
