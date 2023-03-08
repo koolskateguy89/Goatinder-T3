d49aedf64a6a22ca1099f0c5febf2efd08f9987b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-03-08 03:02:12 +0000

    Add manage group chat page (not done)

---
+++ src/pages/chat/[id]/manage.tsx
@@ -0,0 +1,10 @@
+export default function ManageGroupChatPage() {}
+
+// TODO: get id from url
+
+// TODO: only allow gc creator to add & remove members
+// TODO: allow creator to change gc name & image
+// TODO: allow creator to delete gc
+
+// anyone in the gc can access this page, they can see the creator
+// and members and be able to leave the GC
