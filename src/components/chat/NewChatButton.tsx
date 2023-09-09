4f7df531322a04f9ee8d3b4253f0ebfd9724f24d
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-09-09 18:03:32 +0100

    Extract common dialog styling

--- src/components/chat/NewChatButton.tsx
+++ src/components/chat/NewChatButton.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { Dialog } from "@headlessui/react";
 
 import { api } from "utils/api";
-import TransitionDialog from "components/common/TransitionDialog";
+import SimpleTransitionDialog from "components/common/SimpleTransitionDialog";
 
 export type NewChatButtonProps = {
   className: string;
@@ -57,9 +56,11 @@ export default function NewChatButton({ className }: NewChatButtonProps) {
         +
       </button>
 
-      <TransitionDialog isOpen={isOpen} closeModal={closeModal}>
-        <Dialog.Panel className="w-80 rounded-2xl bg-base-100 p-6 shadow-lg">
-          <Dialog.Title as="h3" className="text-xl font-semibold">
+      <SimpleTransitionDialog
+        isOpen={isOpen}
+        closeModal={closeModal}
+        title={
+          <>
             New Chat
             {!users && (
               <span className="text-base font-normal text-gray-500">
@@ -67,42 +68,43 @@ export default function NewChatButton({ className }: NewChatButtonProps) {
                 (loading...)
               </span>
             )}
-          </Dialog.Title>
-
-          <form onSubmit={handleSubmit} className="mt-2 space-y-2">
-            <select
-              name="userId"
-              className="select-bordered select w-full max-w-xs"
-              defaultValue="Select a user"
+          </>
+        }
+      >
+        <form onSubmit={handleSubmit} className="mt-2 space-y-2">
+          <select
+            name="userId"
+            className="select-bordered select w-full max-w-xs"
+            defaultValue="Select a user"
+            disabled={!users}
+          >
+            <option disabled>Select a user</option>
+            {users?.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <div className="flex justify-between">
+            <button
+              type="submit"
+              className="btn-link btn-primary btn"
               disabled={!users}
             >
-              <option disabled>Select a user</option>
-              {users?.map((user) => (
-                <option key={user.id} value={user.id}>
-                  {user.name}
-                </option>
-              ))}
-            </select>
-            <div className="flex justify-between">
-              <button
-                type="submit"
-                className="btn-primary btn-link btn"
-                disabled={!users}
-              >
-                Chat
-              </button>
-              {/* <button
-                type="button"
-                className="btn-primary btn"
-                onClick={createNewGroupChat}
-                disabled={newGroupChatMut.isLoading}
-              >
-                New group chat
-              </button> */}
-            </div>
-          </form>
-        </Dialog.Panel>
-      </TransitionDialog>
+              Chat
+            </button>
+            {/* TODO: uncomment once manage page implemented */}
+            {/* <button
+              type="button"
+              className="btn-primary btn"
+              onClick={createNewGroupChat}
+              disabled={newGroupChatMut.isLoading}
+            >
+              New group chat
+            </button> */}
+          </div>
+        </form>
+      </SimpleTransitionDialog>
     </>
   );
 }
