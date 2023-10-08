b6d662777ef68c210d9007582c0290415ce9c643
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-10-08 20:42:34 +0100

    Fix stats comment colour

--- src/components/profile/Stats.tsx
+++ src/components/profile/Stats.tsx
@@ -9,15 +9,15 @@ export type StatsProps = {
 export default function Stats({ comments, likes, dislikes }: StatsProps) {
   return (
     <span className="flex gap-x-2 [&>*]:w-20 [&>*]:gap-1">
-      <span className="badge badge-lg">
+      <span className="badge badge-neutral badge-lg">
         <span className="sr-only">Number of comments:</span>
         <MdModeComment /> {comments}
       </span>
-      <span className="badge-success badge badge-lg">
+      <span className="badge badge-success badge-lg">
         <span className="sr-only">Number of likes:</span>
         <MdFavorite /> {likes}
       </span>
-      <span className="badge-error badge badge-lg">
+      <span className="badge badge-error badge-lg">
         <span className="sr-only">Number of dislikes:</span>
         <MdClose /> {dislikes}
       </span>
