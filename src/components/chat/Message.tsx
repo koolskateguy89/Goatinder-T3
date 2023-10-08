9e993728fd6462d71cf9666e7f4fdc8d1fabcd58
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-10-08 21:09:08 +0100

    Format message date and include delete button in menu

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import { useSession } from "next-auth/react";
 import type { GroupChatMessage, PrivateMessage, User } from "@prisma/client";
+import { Menu, Transition } from "@headlessui/react";
 import clsx from "clsx";
 
 import { api } from "utils/api";
@@ -10,21 +12,75 @@ type CommonMessage = GroupChatMessage | PrivateMessage;
 
 // /images/stock/photo-1534528741775-53994a69daeb.jpg
 
-export type MessageProps = Pick<CommonMessage, "id" | "content" | "sentAt"> & {
-  sender: Pick<User, "id" | "name" | "image">;
-  onDelete: (messageId: string) => void;
-};
-
 const ONE_DAY_MS = 1000 * 60 * 60 * 24;
 
 function formatDate(date: Date): string {
   const now = Date.now();
 
-  // TODO: hour, time, short date
-  if (now - date.getTime() > ONE_DAY_MS) return date.toISOString();
+  if (now - date.getTime() > ONE_DAY_MS)
+    return date.toLocaleDateString(undefined, {
+      minute: "numeric",
+      hour: "numeric",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
 
-  // TODO: shorter, just hour & time
-  return date.toLocaleTimeString();
+  return date.toLocaleTimeString(undefined, {
+    minute: "numeric",
+    hour: "numeric",
+  });
+}
+
+interface MessageContentProps extends Pick<CommonMessage, "content"> {
+  canDelete: boolean;
+  onDelete(): void;
+}
+
+function MessageContent({ content, canDelete, onDelete }: MessageContentProps) {
+  return !canDelete ? (
+    <div className="chat-bubble">{content}</div>
+  ) : (
+    <Menu as="div" className="relative flex">
+      <Menu.Button className="chat-bubble">{content}</Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          as="ul"
+          className="menu rounded-box absolute right-0 top-full z-10 mt-1 w-52 origin-top-right bg-base-200 p-2 shadow"
+        >
+          {canDelete && (
+            <Menu.Item as="li">
+              {({ close }) => (
+                <button
+                  type="button"
+                  onClick={() => {
+                    onDelete();
+                    close();
+                  }}
+                >
+                  Delete
+                </button>
+              )}
+            </Menu.Item>
+          )}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+export interface MessageProps
+  extends Pick<CommonMessage, "id" | "content" | "sentAt"> {
+  sender: Pick<User, "id" | "name" | "image">;
+  onDelete: (messageId: string) => void;
 }
 
 export default function Message({
@@ -40,7 +96,8 @@ export default function Message({
 
   const { groupChat } = useChatInfo();
 
-  // TODO: rightclick -> menu -> delete [if isMyMessage or iAmAdmin(only for gc
+  // TODO: check if user is gc creator
+  const canDelete = isMyMessage || false;
 
   const deleteMut = api.chat.deleteMessage.useMutation();
 
@@ -51,7 +108,7 @@ export default function Message({
 
   return (
     <article className={clsx("chat", isMyMessage ? "chat-end" : "chat-start")}>
-      {/* TODO?: right click avatar -> menu -> open profile, other options */}
+      {/* TODO?: right click avatar -> menu -> open profile, other options - maybe not */}
       <Avatar
         image={sender.image}
         name={sender.name}
@@ -60,37 +117,19 @@ export default function Message({
           sizes: "2.5rem",
         }}
       />
-      <div className="chat-header">
-        {sender.name}
+      <div className="chat-header space-x-1">
+        <span>{sender.name}</span>
         <time dateTime={sentAt.toISOString()} className="text-xs opacity-50">
           {formatDate(sentAt)}
         </time>
       </div>
-      <div className="chat-bubble">{content}</div>
+      <MessageContent
+        content={content}
+        canDelete={canDelete}
+        onDelete={deleteMessage}
+      />
       {/* TODO: not sure about if we want to show status (seen etc.) */}
       <div className="chat-footer opacity-50">Delivered</div>
-      {isMyMessage && (
-        <button
-          type="button"
-          onClick={deleteMessage}
-          aria-label="Delete message"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      )}
     </article>
   );
 }
