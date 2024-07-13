ebecb24f16765e8d90883a061eff934fd5b0126b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2024-07-13 13:02:42 +0100

    Consolidate common Dialog components

--- src/components/common/SimpleDialog.tsx
+++ src/components/common/SimpleDialog.tsx
@@ -1,19 +1,17 @@
-import { forwardRef } from "react";
+import React, { forwardRef } from "react";
 import { Dialog, type DialogProps } from "@headlessui/react";
 
-export type SimpleDialogProps = Omit<
-  DialogProps<"div">,
-  "open" | "className"
-> & {
-  isOpen: boolean | undefined;
+export type SimpleDialogProps = Omit<DialogProps<"div">, "open" | "className"> &
+  React.PropsWithChildren<{
+    isOpen: boolean | undefined;
 
-  /**
-   * Any arbitrary content to render behind the dialog.
-   */
-  backdrop?: React.ReactNode;
+    title?: React.ReactNode;
 
-  children?: React.ReactNode;
-};
+    /**
+     * Any arbitrary content to render behind the dialog.
+     */
+    backdrop?: React.ReactNode;
+  }>;
 
 /**
  * Wrapper around the Dialog component from Headless UI.
@@ -22,7 +20,7 @@ export type SimpleDialogProps = Omit<
  */
 export default forwardRef<typeof Dialog, SimpleDialogProps>(
   // eslint-disable-next-line prefer-arrow-callback
-  function SimpleDialog({ isOpen, backdrop, children, ...props }, ref) {
+  function SimpleDialog({ isOpen, title, backdrop, children, ...props }, ref) {
     return (
       // @ts-expect-error spread error idk, not worth time resolving
       <Dialog open={isOpen} className="relative z-40" {...props} ref={ref}>
@@ -32,7 +30,15 @@ export default forwardRef<typeof Dialog, SimpleDialogProps>(
         <div className="fixed inset-0 overflow-y-auto">
           {/* Container to center the panel */}
           <div className="flex min-h-full items-center justify-center">
-            {children}
+            <Dialog.Panel className="w-80 rounded-2xl bg-base-100 p-6 shadow-lg">
+              {title && (
+                <Dialog.Title as="h3" className="text-xl font-semibold">
+                  {title}
+                </Dialog.Title>
+              )}
+
+              {children}
+            </Dialog.Panel>
           </div>
         </div>
       </Dialog>
