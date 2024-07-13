783184d4658d2c4fdd70b1e0ddb7c5faba8af22d
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2024-07-13 13:02:42 +0100

    Consolidate common Dialog components

--- src/components/common/SimpleTransitionDialog.tsx
+++ src/components/common/SimpleTransitionDialog.tsx
@@ -1,18 +1,19 @@
-import { Dialog } from "@headlessui/react";
+import { Fragment } from "react";
+import { Transition } from "@headlessui/react";
 
-import TransitionDialog, {
-  type TransitionDialogProps,
-} from "components/common/TransitionDialog";
+import SimpleDialog, {
+  type SimpleDialogProps,
+} from "components/common/SimpleDialog";
 
 export interface SimpleTransitionDialogProps {
   isOpen: boolean;
   closeModal: () => void;
-  title?: React.ReactNode;
+  title?: SimpleDialogProps["title"];
   children: React.ReactNode;
 }
 
 /**
- * `TransitionDialog` with common dialog panel styling.
+ * Wrapper around the Transition and Dialog components from Headless UI with common dialog styling.
  */
 export default function SimpleTransitionDialog({
   isOpen,
@@ -21,16 +22,39 @@ export default function SimpleTransitionDialog({
   children,
 }: SimpleTransitionDialogProps) {
   return (
-    <TransitionDialog isOpen={isOpen} closeModal={closeModal}>
-      <Dialog.Panel className="w-80 rounded-2xl bg-base-100 p-6 shadow-lg">
-        {title && (
-          <Dialog.Title as="h3" className="text-xl font-semibold">
-            {title}
-          </Dialog.Title>
-        )}
-
-        {children}
-      </Dialog.Panel>
-    </TransitionDialog>
+    <Transition show={isOpen} as={Fragment}>
+      <SimpleDialog
+        // We use undefined 'open' here because setting it makes the dialog not transition at all when closing
+        isOpen={undefined}
+        onClose={closeModal}
+        title={title}
+        backdrop={
+          // The backdrop, rendered as a fixed sibling to the panel container
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="fixed inset-0 bg-black/30" aria-hidden />
+          </Transition.Child>
+        }
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {children}
+        </Transition.Child>
+      </SimpleDialog>
+    </Transition>
   );
 }
